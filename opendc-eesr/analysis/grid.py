380c6734146e6cc6650827c5c6e3368fcb82a738
philippsommer27 <philippsommerhalter@gmail.com>
2022-07-07 00:43:16 +0200

    Classify Grid Analysis

--- opendc-eesr/analysis/grid.py
+++ opendc-eesr/analysis/grid.py
@@ -2,177 +2,208 @@ import pandas as pd
 from entsoe import mappings
 from entsoe_caching import cached_query_generation, cached_query_crossborder_flows, cached_query_wind_and_solar_forecast
 
-def calc_total_prod(df: pd.DataFrame):
-    if 'total_prod' in df:
-        df.drop('total_prod', axis=1, inplace=True)
+class GridAnalysis:
 
-    df['total_prod'] = df.loc[:, df.columns.isin(list(PROD_CAT))].sum(axis=1)
+    def __init__(self, df_dc, start: pd.Timestamp, end: pd.Timestamp, key_path, country='NL', get_bordering=True) -> None:
+        self.df_dc = df_dc
+        self.start = start
+        self.end = end
+        self.keypath = key_path
+        self.coutry = country
+        self.get_bordering = get_bordering
+        self.df = self.fetch_energy_prod()
+        self.merge_dc_grid()
 
-def _fetch_cross_border(df: pd.DataFrame, key_path, country, start: pd.Timestamp, end:pd.Timestamp):
+    def calc_total_prod(self):
+        if 'total_prod' in self.df:
+            self.df.drop('total_prod', axis=1, inplace=True)
 
-    for neighbour_code in mappings.NEIGHBOURS['country']:
-        neighbour_flow = cached_query_crossborder_flows(country, neighbour_code, start, end)
-        neighbour_prod = fetch_energy_prod(start, end, key_path, neighbour_code)
+        self.df['total_prod'] = self.df.loc[:, self.df.columns.isin(list(PROD_CAT))].sum(axis=1)
 
-        assert(neighbour_flow.shape[0] == neighbour_prod.shape[0], "Fetch Border Error: Flow data and production data do not match")
+    def _fetch_cross_border(self):
+
+        for neighbour_code in mappings.NEIGHBOURS['country']:
+            neighbour_flow = cached_query_crossborder_flows(self.country, self.neighbour_code, self.start, self.end)
+            neighbour_prod = self.fetch_energy_prod(self.start, self.end, self.key_path, neighbour_code)
+
+            assert(neighbour_flow.shape[0] == neighbour_prod.shape[0], "Fetch Border Error: Flow data and production data do not match")
+            
+            columns = neighbour_prod.columns()
+            neighbour_prod['total_prod'] = neighbour_prod.sum(axis=1)
+
+            for column in columns:
+                neighbour_prod[column + '_perc'] = neighbour_prod[column] / neighbour_prod['total_prod']
+                neighbour_prod[column + '_flow'] = neighbour_prod[column + '_perc'] * neighbour_flow
+
+                if column in self.df.columns:
+                    self.df[column] = self.df[column] + neighbour_prod[column + '_flow']
+                else:
+                    self.df[column] = neighbour_prod[column + '_flow']
         
-        columns = neighbour_prod.columns()
-        neighbour_prod['total_prod'] = neighbour_prod.sum(axis=1)
+
+    def fetch_energy_prod(self, country):
+        
+        df = cached_query_generation(country, self.start, self.end, self.key_path)
+
+        # Cleanup
+
+        df = df.fillna(0)
+        df.loc[:, (df != 0).any(axis=0)]
+        df.drop(list(df.filter(regex = 'Consumption')), axis = 1, inplace = True)
+
+        df.columns = df.columns.droplevel(-1)
+
+        if self.get_bordering():
+            self._fetch_cross_border(df, self.country, self.start, self.end)
+
+        return df
+
+    def merge_dc_grid(self):
+        assert(self.df.index[0] == self.df_dc.index[0], "DC start time does not match grid start time")
+        assert(self.df.index[-1] == self.df_dc.index[-1], "DC end time does not match grid end time")
+        assert(len(self.df) == len(self.df_dc), "Timeframes do not match")
+
+        return pd.concat([self.df, self.df_dc], axis=1)
+
+    def compute_energy_prod_ratios(self):
+        columns = self.df.columns()
+
+        self.calc_total_prod()
+
+        self.df['renewable_total'] = 0
+        self.df['non_renewable_total'] = 0
+        self.df['green_total'] = 0
+        self.df['non_green_total'] = 0
 
         for column in columns:
-            neighbour_prod[column + '_perc'] = neighbour_prod[column] / neighbour_prod['total_prod']
-            neighbour_prod[column + '_flow'] = neighbour_prod[column + '_perc'] * neighbour_flow
+            if column in PROD_CAT.keys():
+                if PROD_CAT[column]['renewable']:
+                    self.df['renewable_total'] += self.df[column]
+                else:
+                    self.df['non_renewable_total'] += self.df[column]
 
-            if column in df.columns:
-                df[column] = df[column] + neighbour_prod[column + '_flow']
-            else:
-                df[column] = neighbour_prod[column + '_flow']
+                if PROD_CAT[column]['green']:
+                    self.df['green_total'] += self.df[column]
+                else:
+                    self.df['non_green_total'] += self.df[column]
+
+        self.df['renewable_perc'] = self.df['renewable_total'] / self.df['total_prod']
+        self.df['non_renewable_perc'] = self.df['non_renewable_total'] / self.df['total_prod']
+        self.df['green_perc'] = self.df['green_total'] / self.df['total_prod']
+        self.df['non_green_perc'] = self.df['non_green_total'] / self.df['total_prod']
+
+
+    def compute_dc_energy_prod_ratios(self):
+        columns = self.df.columns()
+
+        self.calc_total_prod()
+
+        self.df['dc_renewable_total'] = 0
+        self.df['dc_non_renewable_total'] = 0
+        self.df['dc_green_total'] = 0
+        self.df['dc_non_green_total'] = 0
+
+        for column in columns:
+            if 'dc_cons_' in column:
+                prod_type = column[8:-1]
+                if PROD_CAT[prod_type]['renewable']:
+                    self.df['dc_renewable_total'] += self.df[column]
+                else:
+                    self.df['dc_non_renewable_total'] += self.df[column]
+
+
+                if PROD_CAT[prod_type]['green']:
+                    self.df['dc_green_total'] += self.df[column]
+                else:
+                    self.df['dc_non_green_total'] += self.df[column]
+
+
+    def compute_dc_cons_by_type_naive(self):
+        assert('dc_power_total' in self.df, "Dataframe does not contain data center power consumption, consider calling merge_dc_grid()")
+
+        self.calc_total_prod()
+
+        for column in self.df.columns():
+            if column in PROD_CAT.keys():
+                self.df['dc_cons_' + column] = (self.df[column] / self.df['total_prod']) * self.df['dc_power_total']
+
+    def compute_apcren(self):
+        assert('dc_power_total' in self.df, "Dataframe does not contain DC power usage")
+        assert('renewable_total' in self.df, "Dataframe does not contain grid renewable consumption")
+
+        dc_sum_consumption_mw = self.df['dc_power_total'] / 1000
+        grid_sum_renewable_production = self.df['renewable_total']
+        K = dc_sum_consumption_mw.sum() / grid_sum_renewable_production.sum()
+        
+        numerator = abs((K * grid_sum_renewable_production) - dc_sum_consumption_mw).sum()
+        APCren = 1 - (numerator / dc_sum_consumption_mw)
+
+        return APCren
+
+
+    def compute_total_co2(self, assume):
+        if assume == 'best':
+            co2_df = pd.read_csv("opendc-eesr/analysis/data/LCA_CO2_BEST.csv")
+        elif assume == 'worst':
+            co2_df = pd.read_csv("opendc-eesr/analysis/data/LCA_CO2_Worst.csv")
+
+        self.df['total_co2'] = 0
+
+        for column in self.df.columns():
+            if 'dc_cons_' in column:
+                self.df['total_co2'] = self.df['total_co2'] + (co2_df.loc[[column[8:-1], 'CO2 [gCO2/kWh]']] * self.df[column])
+
+        return self.df['total_co2'].sum()
+  
+
+    def compute_power_cost(self):
+        pass
+
+
+    def compute_gec_green(self):
+        return self.df['cd_green_total'].sum() / self.df['dc_power_total'].sum()
+
+    def compute_gec_renewable(self):
+        return self.df['dc_renewable_total'].sum() / self.df['dc_power_total'].sum()
+
+    def compute_cue(self):
+        self.df['cue'] = self.df['total_co2'] / self.df['it_power_total']
+
+        return self.df['cue'].mean()
+
+    def compute_nenr(self):
+        pass
+
+    def analyze(self, out):
+        self.fetch_energy_prod()
+        self.compute_dc_cons_by_type_naive()
+        self.compute_energy_prod_ratios()
+        self.compute_dc_energy_prod_ratios()
+        APCren = self.compute_apcren()
+        CO2 = self.compute_total_co2()
+        GEC = self.compute_gec_renewable()
+        CUE = self.compute_cue
+
+        res = {
+            "builtin_metrics" : {
+                "CO2" : CO2,
+                "GEC" : GEC,
+                "APCr" : APCren,
+                "CUE" : CUE
+            }
+        }
+
+        return res
+
+
         
 
-def fetch_energy_prod(start: pd.Timestamp, end: pd.Timestamp, key_path, country='NL', get_bordering=True):
-    
-    df = cached_query_generation(country, start, end, key_path)
-
-    # Cleanup
-
-    df = df.fillna(0)
-    df.loc[:, (df != 0).any(axis=0)]
-    df.drop(list(df.filter(regex = 'Consumption')), axis = 1, inplace = True)
-
-    df.columns = df.columns.droplevel(-1)
-
-    if get_bordering():
-        _fetch_cross_border(df, country, start, end)
-
-    return df
-
-def fetch_generation_forecast_csv(start: pd.Timestamp, end: pd.Timestamp, key_path, out, country='NL'):
+def fetch_generation_forecast_csv(start: pd.Timestamp, end: pd.Timestamp, out, country='NL'):
 
     df = cached_query_wind_and_solar_forecast(country_code=country, start=start, end=end)
     df = df.fillna(0)
 
     df.to_csv(out)
-
-def merge_dc_grid(df_grid: pd.DataFrame, df_dc: pd.DataFrame):
-    assert(df_grid.index[0] == df_dc.index[0], "DC start time does not match grid start time")
-    assert(df_grid.index[-1] == df_dc.index[-1], "DC end time does nto match grid end time")
-    pass
-
-def compute_energy_prod_ratios(df: pd.DataFrame):
-    columns = df.columns()
-
-    calc_total_prod(df)
-
-    df['renewable_total'] = 0
-    df['non_renewable_total'] = 0
-    df['green_total'] = 0
-    df['non_green_total'] = 0
-
-    for column in columns:
-        if column in PROD_CAT.keys():
-            if PROD_CAT[column]['renewable']:
-                df['renewable_total'] += df[column]
-            else:
-                df['non_renewable_total'] += df[column]
-
-
-            if PROD_CAT[column]['green']:
-                df['green_total'] += df[column]
-            else:
-                df['non_green_total'] += df[column]
-
-    df['renewable_perc'] = df['renewable_total'] / df['total_prod']
-    df['non_renewable_perc'] = df['non_renewable_total'] / df['total_prod']
-    df['green_perc'] = df['green_total'] / df['total_prod']
-    df['non_green_perc'] = df['non_green_total'] / df['total_prod']
-
-
-def compute_dc_energy_prod_ratios(df: pd.DataFrame):
-    columns = df.columns()
-
-    calc_total_prod(df)
-
-    df['dc_renewable_total'] = 0
-    df['dc_non_renewable_total'] = 0
-    df['dc_green_total'] = 0
-    df['dc_non_green_total'] = 0
-
-    for column in columns:
-        if 'dc_cons_' in column:
-            prod_type = column[8:-1]
-            if PROD_CAT[prod_type]['renewable']:
-                df['dc_renewable_total'] += df[column]
-            else:
-                df['dc_non_renewable_total'] += df[column]
-
-
-            if PROD_CAT[prod_type]['green']:
-                df['dc_green_total'] += df[column]
-            else:
-                df['dc_non_green_total'] += df[column]
-
-
-
-def compute_dc_cons_by_type_naive(df: pd.DataFrame):
-    assert('dc_power_total' in df, "Dataframe does not contain data center power consumption, consider calling merge_dc_grid()")
-
-    calc_total_prod(df)
-
-    for column in df.columns():
-        if column in PROD_CAT.keys():
-            df['dc_cons_' + column] = (df[column] / df['total_prod']) * df['dc_power_total']
-
-def compute_apcren(df: pd.DataFrame):
-    assert('dc_power_total' in df, "Dataframe does not contain DC power usage")
-    assert('renewable_total' in df, "Dataframe does not contain grid renewable consumption")
-
-    dc_sum_consumption_mw = df['dc_power_total'] / 1000
-    grid_sum_renewable_production = df['renewable_total']
-    K = dc_sum_consumption_mw.sum() / grid_sum_renewable_production.sum()
-    
-    numerator = abs((K * grid_sum_renewable_production) - dc_sum_consumption_mw).sum()
-    APCren = 1 - (numerator / dc_sum_consumption_mw)
-
-    return APCren
-
-
-def compute_total_co2(df: pd.DataFrame, assume):
-    if assume == 'best':
-        co2_df = pd.read_csv("opendc-eesr/analysis/data/LCA_CO2_BEST.csv")
-    elif assume == 'worst':
-        co2_df = pd.read_csv("opendc-eesr/analysis/data/LCA_CO2_Worst.csv")
-
-    df['total_co2'] = 0
-
-    for column in df.columns():
-        if 'dc_cons_' in column:
-            df['total_co2'] = df['total_co2'] + (co2_df.loc[[column[8:-1], 'CO2 [gCO2/kWh]']] * df[column])
-
-    return df['total_co2'].sum()
-   
-
-def compute_power_cost(df: pd.DataFrame):
-    pass
-
-def compute_gec_green(df: pd.DataFrame):
-    return df['cd_green_total'].sum() / df['dc_power_total'].sum()
-
-def compute_gec_renewable(df: pd.DataFrame):
-    return df['dc_renewable_total'].sum() / df['dc_power_total'].sum()
-
-def compute_cue(df: pd.DataFrame):
-    df['cue'] = df['total_co2'] / df['it_power_total']
-
-    return df['cue'].mean()
-
-def compute_nenr():
-    pass
-
-def compute_sustainability_metrics(df: pd.DataFrame, out):
-    pass
-
-def save_df():
-    pass
 
 PROD_CAT = {
     'Mixed' : {'renewable' : False, 'green' : False},
@@ -199,7 +230,4 @@ PROD_CAT = {
 }
 
 if __name__ == "__main__":
-    start = pd.Timestamp('20181123', tz='Europe/Amsterdam')
-    end = pd.Timestamp('20190111', tz='Europe/Amsterdam')
-    fetch_energy_prod(start, end, key_path="G:\My Drive\VU Amsterdam\Year 3\Bachelor Project\entsoe_token.txt")
-
+    pass
